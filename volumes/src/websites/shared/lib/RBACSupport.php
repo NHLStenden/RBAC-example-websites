3f3766a4f1991d96054d47f9e7424103b167e0e4
Martin Molema <Martin.Molema@nhlstenden.com>
2025-03-13 08:23:13 +0100

    Better error handling when wrong user is searched

--- volumes/src/websites/shared/lib/RBACSupport.php
+++ volumes/src/websites/shared/lib/RBACSupport.php
@@ -25,131 +25,139 @@ const Permission_Marketing_Approve_Campaign = 'Marketing_Approve_Campaign';
 class RBACSupport
 {
 
-  private PDO             $db;
-  private LDAP\Connection $lnk;
+    private PDO $db;
+    private LDAP\Connection $lnk;
 
-  public string $username;
-  public string $userDN;
-  public array  $groups;
-  public array  $permissions;
+    public string $username;
+    public string $userDN;
+    public array $groups;
+    public array $permissions;
 
-  public array $userInfoLDAP;
+    public array $userInfoLDAP;
 
-  public function __construct(string $username)
-  {
-    $this->db = new PDO('mysql:host=iam-example-db-server;dbname=IAM;', "student", "test1234");
-    try {
-      $this->lnk = ConnectAndCheckLDAP();
-    } catch (Exception $ex) {
-      die($ex->getMessage());
-    }
-    $this->username     = $username;
-    $this->userDN       = '';
-    $this->groups       = array();
-    $this->permissions  = array();
-    $this->userInfoLDAP = array();
-  }
-
-  /**
-   * This function will convert the username to a DN, get the group memberships and then convert these in permissions
-   * @return array | null
-   */
-  public function process(): bool
-  {
-    try {
-      $this->getUserDN();
-      if ($this->getGroupsForUser()) {
-        $this->getPermissions();
-        return true;
-      }
-    } catch (Exception $ex) {
-      error_log($ex->getMessage());
-    }
-    return false;
-  }
-
-  /**
-   * @return bool
-   */
-  public function getUserDN(): bool
-  {
-    try {
-      $this->userDN = GetUserDNFromUID($this->lnk, $this->username);
-      $this->getUserInformation();
-      return true;
-    } catch (Exception $ex) {
-      error_log($ex->getMessage());
+    public function __construct(string $username)
+    {
+        $this->db = new PDO('mysql:host=iam-example-db-server;dbname=IAM;', "student", "test1234");
+        try {
+            $this->lnk = ConnectAndCheckLDAP();
+        } catch (Exception $ex) {
+            die($ex->getMessage());
+        }
+        $this->username = $username;
+        $this->userDN = '';
+        $this->groups = array();
+        $this->permissions = array();
+        $this->userInfoLDAP = array();
     }
 
-    return false;
-  }
-
-  public function getUserInformation(): array|null
-  {
-    try {
-      $ldapInfo = GetUserDataFromDN($this->lnk, $this->userDN);
-
-      $result = [];
-      $nrOfItems = $ldapInfo['count'];
-      for($i = 0; $i < $nrOfItems; $i++) {
-        $key = $ldapInfo[$i];
-        $value = $ldapInfo[$key][0];
-        $result[$key] = $value;
-      }
-      $result['dn'] = $ldapInfo['dn'];
-
-      $this->userInfoLDAP = $result;
-      return $this->userInfoLDAP;
-    } catch (Exception $ex) {
-      error_log($ex->getMessage());
-
-    }
-    return null;
-  }
-
-  /**
-   * @return bool
-   */
-  public function getGroupsForUser(): bool
-  {
-    try {
-      $this->groups = GetAllLDAPGroupMemberships($this->lnk, $this->userDN);
-      return true;
-    } catch (Exception $ex) {
-      error_log($ex->getMessage());
-    }
-    return false;
-  }
-
-  /**
-   * @return array
-   */
-  public function getPermissions(): array
-  {
-    $allGroupsForSQL = implode(", ",
-      array_map(function ($g) {
-        return "'$g'";
-      }, $this->groups));
-
-    $SQL = "SELECT * FROM vw_Role_Permissions WHERE dn in ({$allGroupsForSQL})";
-
-    try {
-      $stmt = $this->db->prepare($SQL);
-      $stmt->execute();
-      $this->permissions = [];
-      foreach ($stmt as $row) {
-        $this->permissions[$row["permission_code"]] = $row;
-      }
-    } catch (PDOException $ex) {
-      echo $ex->getMessage();
+    /**
+     * This function will convert the username to a DN, get the group memberships and then convert these in permissions
+     * @return array | null
+     */
+    public function process(): bool
+    {
+        try {
+            if ($this->getUserDN() && $this->getGroupsForUser()) {
+                $this->getPermissions();
+                return true;
+            }
+            else {
+                return false;
+            }
+        } catch (Exception $ex) {
+            error_log($ex->getMessage());
+        }
+        return false;
     }
 
-    return $this->permissions;
-  }
+    /**
+     * @return bool
+     */
+    public function getUserDN(): bool
+    {
+        try {
+            $userDN = GetUserDNFromUID($this->lnk, $this->username);
+            if (!is_null($userDN)) {
+                $this->userDN = $userDN;
+                $this->getUserInformation();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception $ex) {
+            error_log($ex->getMessage());
+        }
 
-  public function has(string $permission): bool {
-    return array_key_exists($permission, $this->permissions);
-  }
+        return false;
+    }
+
+    public function getUserInformation(): array|null
+    {
+        try {
+            $ldapInfo = GetUserDataFromDN($this->lnk, $this->userDN);
+
+            $result = [];
+            $nrOfItems = $ldapInfo['count'];
+            for ($i = 0; $i < $nrOfItems; $i++) {
+                $key = $ldapInfo[$i];
+                $value = $ldapInfo[$key][0];
+                $result[$key] = $value;
+            }
+            $result['dn'] = $ldapInfo['dn'];
+
+            $this->userInfoLDAP = $result;
+            return $this->userInfoLDAP;
+        } catch (Exception $ex) {
+            error_log($ex->getMessage());
+
+        }
+        return null;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getGroupsForUser(): bool
+    {
+        try {
+            $this->groups = GetAllLDAPGroupMemberships($this->lnk, $this->userDN);
+            return true;
+        } catch (Exception $ex) {
+            error_log($ex->getMessage());
+        }
+        return false;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPermissions(): array
+    {
+        $allGroupsForSQL = implode(", ",
+            array_map(function ($g) {
+                return "'$g'";
+            }, $this->groups));
+
+        $SQL = "SELECT * FROM vw_Role_Permissions WHERE dn in ({$allGroupsForSQL})";
+
+        try {
+            $stmt = $this->db->prepare($SQL);
+            $stmt->execute();
+            $this->permissions = [];
+            foreach ($stmt as $row) {
+                $this->permissions[$row["permission_code"]] = $row;
+            }
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+        }
+
+        return $this->permissions;
+    }
+
+    public function has(string $permission): bool
+    {
+        return array_key_exists($permission, $this->permissions);
+    }
 
 
 }
