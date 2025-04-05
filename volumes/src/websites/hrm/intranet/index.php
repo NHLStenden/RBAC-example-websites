c655850a18998f83b994c32cffd1de9994c87cad
Martin Molema <martin.molema@nhlstenden.com>
2025-04-05 19:27:33 +0200

    better table: smart formatted date and added last_sync value

--- volumes/src/websites/hrm/intranet/index.php
+++ volumes/src/websites/hrm/intranet/index.php
@@ -2,6 +2,32 @@
 include_once '../../shared/lib/RBACSupport.php';
 include_once '../../shared/partials/header.php';
 
+function formatDate($date) {
+  // Zet de datum om naar een DateTime-object
+  $dateTime = new DateTime($date);
+  $now = new DateTime();
+
+  // Vandaag
+  if ($dateTime->format('Y-m-d') === $now->format('Y-m-d')) {
+    return $dateTime->format('H:i');  // Alleen tijd
+  }
+
+  // Maximaal een week oud
+  $interval = $now->diff($dateTime);
+  if ($interval->days <= 7) {
+    return $dateTime->format('l H:i');  // Naam van de dag en tijd
+  }
+
+  // Zelfde jaar
+  if ($dateTime->format('Y') === $now->format('Y')) {
+    return $dateTime->format('m-d H:i');  // Maand, dag en tijd
+  }
+
+  // Anders volledige datum in Nederlands formaat
+  return $dateTime->format('l j F Y H:i');  // Volledige datum met dagnaam volledig uitgeschreven
+}
+
+
 $rbac = new RBACSupport($_SERVER["AUTHENTICATE_UID"]);
 if (!$rbac->process()) {
     die('Could not connect to RBAC server.');
@@ -40,27 +66,28 @@ $medewerkers = $pdo->query("SELECT * FROM medewerkers ORDER BY achternaam, voorn
             <tr>
                 <th>ID</th>
                 <th>Naam</th>
+                <th>Voornaam</th>
                 <th>Organisatie</th>
                 <th>Telefoon</th>
                 <th>Kamernummer</th>
                 <th>Postcode</th>
-                <th>Team</th>
                 <th>Functie</th>
+                <th>Update</th>
                 <th>Acties</th>
-                <th>Laatst bijgewerkt</th>
             </tr>
             </thead>
             <tbody>
             <?php foreach ($medewerkers as $medewerker): ?>
                 <tr>
                     <td><?= $medewerker['personeelsnummer'] ?></td>
-                    <td><?= htmlspecialchars($medewerker['voornaam'] . ' ' . $medewerker['achternaam']) ?></td>
+                    <td><?= htmlspecialchars($medewerker['achternaam']) ?></td>
+                    <td><?= htmlspecialchars($medewerker['voornaam']) ?></td>
                     <td><?= htmlspecialchars($medewerker['team']) ?></td>
                     <td><?= htmlspecialchars($medewerker['telefoonnummer']) ?></td>
                     <td><?= htmlspecialchars($medewerker['kamernummer']) ?></td>
                     <td><?= htmlspecialchars($medewerker['postcode']) ?></td>
                     <td><?= htmlspecialchars($medewerker['functie']) ?></td>
-                    <td><?= htmlspecialchars($medewerker['last_sync']) ?></td>
+                    <td><?= htmlspecialchars(formatDate($medewerker['last_sync'])) ?></td>
                     <td>
                         <a class="button" href="form.php?id=<?= $medewerker['idMedewerker'] ?>">Bewerken</a>
                         <a class="button" href="delete.php?id=<?= $medewerker['idMedewerker'] ?>"
