f8e233aa21a8a1523ea639d68e62664fd4951f93
Martin Molema <martin.molema@nhlstenden.com>
2025-04-06 13:18:23 +0200

    new script to assign roles to users

--- volumes/src/websites/admin/intranet/AssignRoleToUser.php
+++ volumes/src/websites/admin/intranet/AssignRoleToUser.php
@@ -1,1 +1,43 @@
 <?php
+
+include_once '../../shared/lib/RBACSupport.php';
+include_once '../../shared/partials/header.php';
+include_once '../../shared/lib/ldap_support.inc.php';
+
+$rbac = new RBACSupport($_SERVER["AUTHENTICATE_UID"]);
+if (!$rbac->process()) {
+  die('Could not connect to RBAC server.');
+}
+if (!$rbac->has(Permission_Admin_Panel)) {
+  echo "Not allowed to open the Admin panel\n";
+  die();
+}
+
+$lnk     = ConnectAndCheckLDAP();
+$user_dn = $_POST['user'];
+$role_dn = $_POST['role'];
+
+$result = AssignUserToRole($lnk, $role_dn, $user_dn);
+
+if ($result) {
+  header('location: /intranet', 301);
+  die();
+}
+
+$error = ldap_error($lnk);
+?>
+
+<!doctype html>
+<html lang="NL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cannot authorise</title>
+</head>
+<body>
+<p>Kan de autorisatie niet toewijzen!</p>
+<p><?= $error ?></p>
+</body>
+</html>
--- volumes/src/websites/admin/intranet/AssignUserToRoleForm.php
+++ volumes/src/websites/admin/intranet/AssignUserToRoleForm.php
@@ -1,6 +1,5 @@
 <?php
 
-
 include_once '../../shared/lib/RBACSupport.php';
 include_once '../../shared/partials/header.php';
 include_once '../../shared/lib/ldap_support.inc.php';
@@ -14,17 +13,19 @@ if (!$rbac->has(Permission_Admin_Panel)) {
   die();
 }
 
-$lnk            = ConnectAndCheckLDAP();
-$users_staff    = GetAllUsersInDN($lnk, 'ou=Staff,dc=NHLStenden,dc=com');
-$users_teachers = GetAllUsersInDN($lnk, 'ou=Teachers,ou=Opleidingen,dc=NHLStenden,dc=com');
-$roles          = GetAllRolesInDN($lnk, "ou=roles,dc=NHLStenden,dc=com");
+$lnk                        = ConnectAndCheckLDAP();
+$users_staff['HRM']         = GetAllUsersInDN($lnk, 'ou=HRM,ou=Staff,dc=NHLStenden,dc=com');
+$users_staff['Marketing']   = GetAllUsersInDN($lnk, 'ou=Marketing,ou=Staff,dc=NHLStenden,dc=com');
+$users_staff['ICT Support'] = GetAllUsersInDN($lnk, 'ou=ICT Support,ou=Staff,dc=NHLStenden,dc=com');
+$users_staff['Docenten']    = GetAllUsersInDN($lnk, 'ou=Teachers,ou=Opleidingen,dc=NHLStenden,dc=com');
+$roles                      = GetAllRolesInDN($lnk, "ou=roles,dc=NHLStenden,dc=com");
 
 ?>
 <html lang="NL">
 <head>
     <title>Admin Panel | Attestation - Rollen</title>
     <link href="css/globals.css" rel="stylesheet">
-    <link href="css/index.css" rel="stylesheet">
+    <link href="css/AssignUserToRoleForm.css" rel="stylesheet">
     <link href="css/header.css" rel="stylesheet">
     <link href="css/attestation.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="../favicon.png">
@@ -37,31 +38,31 @@ $roles          = GetAllRolesInDN($lnk, "ou=roles,dc=NHLStenden,dc=com");
       <?= showheader(Websites::WEBSITE_ADMIN, basename(__FILE__), $rbac) ?>
     </article>
     <article>
-        <form>
-            <label for="role">Rol:</label>
-            <select name="role" id="role">
-              <?php foreach ($roles as $role) : ?>
-                  <option value="<?= $role['dn'] ?>" ><?= $role['cn'] ?></option>
-              <?php endforeach; ?>
+        <fieldset>
+            <legend>Nieuwe autorisatie aanvraag verwerken</legend>
+            <form method="post" action="AssignRoleToUser.php">
+                <label for="role">Rol:</label>
+                <select name="role" id="role">
+                  <?php foreach ($roles as $role) : ?>
+                      <option value="<?= $role['dn'] ?>"><?= $role['cn'] ?></option>
+                  <?php endforeach; ?>
 
-            </select>
-            <br>
-            <label for="user">Gebruiker:</label>
-            <select name="user" id="user">
-                <optgroup label="Staf">
-                  <?php foreach ($users_staff as $user) : ?>
-                      <option value="<?= $user['dn'] ?>"><?= $user['sn'] . "," . $user['givenName'] ?></option>
+                </select>
+                <br>
+                <label for="user">Gebruiker:</label>
+                <select name="user" id="user">
+                  <?php foreach ($users_staff as $key => $department): ?>
+                      <optgroup label="<?= $key ?>">
+                        <?php foreach ($department as $user) : ?>
+                            <option value="<?= $user['dn'] ?>"><?= $user['sn'] . "," . $user['givenName'] ?></option>
+                        <?php endforeach; ?>
+                      </optgroup>
                   <?php endforeach; ?>
-                </optgroup>
-                <optgroup label="Docenten">
-                  <?php foreach ($users_teachers as $user) : ?>
-                      <option value="<?= $user['dn'] ?>"><?= $user['sn'] . "," . $user['givenName'] ?></option>
-                  <?php endforeach; ?>
-                </optgroup>
-            </select>
-            <br>
-            <button type="submit">Autoriseer</button>
-        </form>
+                </select>
+                <br>
+                <button type="submit">Autoriseer</button>
+            </form>
+        </fieldset>
     </article>
 
 </main>
