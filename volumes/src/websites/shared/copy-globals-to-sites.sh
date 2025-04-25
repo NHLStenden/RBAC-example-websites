e309f1f3632c86121ab05f5d660018a487a5a7be
Martin Molema <Martin.Molema@nhlstenden.com>
2025-04-25 15:49:55 +0200

    added copy for favicon

--- volumes/src/websites/shared/copy-globals-to-sites.sh
+++ volumes/src/websites/shared/copy-globals-to-sites.sh
@@ -3,3 +3,8 @@ cp css/*.css ../marketing/intranet/css
 cp css/*.css ../admin/intranet/css
 cp css/*.css ../sharepoint/intranet/css
 cp css/*.css ../grades/intranet/css
+
+cp favicon.png ../marketing
+cp favicon.png ../admin
+cp favicon.png ../sharepoint
+cp favicon.png ../grades
