4bcc9c9713a9eea4059fa1d574f0180d6cccc6ac
Martin Molema <martin.molema@nhlstenden.com>
2025-05-06 19:31:51 +0200

    Moved file and created function to standardise DB access parameters

--- volumes/src/websites/shared/lib/db.php
+++ volumes/src/websites/shared/lib/db.php
@@ -1,9 +1,28 @@
 <?php
 
-function ConnectDatabase(): PDO
+function ConnectDatabaseIAM(): PDO
 {
 
-// config.php - Database configuratie
+// db.php - Database configuratie
+  $host     = 'iam-example-db-server';
+  $dbname   = 'IAM';
+  $username = 'student';
+  $password = 'test1234';
+
+  try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $e) {
+    die("Database verbinding mislukt: " . $e->getMessage());
+  }
+
+  return $pdo;
+}
+
+function ConnectDatabaseHRM(): PDO
+{
+
+// db.php - Database configuratie
   $host     = 'iam-example-hrm-server';
   $dbname   = 'HRM';
   $username = 'admin';
