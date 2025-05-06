25ff570fffe0ee1ce5b39ecd2c0ff17730a6da5d
Martin Molema <martin.molema@nhlstenden.com>
2025-05-06 22:11:59 +0200

    Added new files to split config

---
+++ provisioning/src/configdb.py
@@ -0,0 +1,18 @@
+
+# Configuratie voor MariaDB
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "admin",
+    "port": 3307,
+    "password": "Test1234!",
+    "database": "HRM",
+}
+
+# Configuratie voor MariaDB IAM Database
+DB_CONFIG_AUDITTRAIL = {
+    "host": "localhost",
+    "user": "student",
+    "port": 3306,
+    "password": "test1234",
+    "database": "IAM",
+}
