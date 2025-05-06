850a2f4adfcd4f92b9a089c522f9278acc9dc6a7
Martin Molema <Martin.Molema@nhlstenden.com>
2025-05-06 14:29:00 +0200

    new parameters for static linking

--- _resources/support/convert-crlf/compile.bat
+++ _resources/support/convert-crlf/compile.bat
@@ -1,2 +1,2 @@
 REM Compile for windows using MSYS2 and G++
-g++ -std=c++17 -o ..\..\..\convert-cr-lf-win.exe convert-cr-lf.cpp
+g++ -std=c++17 -static -static-libstdc++  -o ..\..\..\convert-cr-lf-win.exe convert-cr-lf.cpp
--- _resources/support/convert-crlf/compile.sh
+++ _resources/support/convert-crlf/compile.sh
@@ -1,2 +1,2 @@
-REM This will compile for Linux if using WSL!
-g++ -std=c++17 -o ../../../convert-cr-lf-wsl convert-cr-lf.cpp
+# This will compile for Linux if using WSL!
+g++ -std=c++17 -static -o ../../../convert-cr-lf-wsl convert-cr-lf.cpp
